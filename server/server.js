80cb27f92d2e945a9bcb753c2387509d95f99c82
Jackson Spindle <jacksonmilesspindle@gmail.com>
2023-06-19 12:45:18 -0600

    bug fix

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://stirring-dusk-267740.netlify.app"
+    "https://main--stirring-dusk-267740.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
